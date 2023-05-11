1bb4323da4eea88f2d8a7f9ed32321e654e76667
Peter Szabo <pts@fazekas.hu>
2023-05-11 13:19:23 +0200

    added -O0 comparison test for test_log_i586.sh

--- test_log_i586.sh
+++ test_log_i586.sh
@@ -4,7 +4,13 @@ set -ex
 CFLAGS="${*:-}"
 nasm-0.98.39 $CFLAGS -O999999999 -w+orphan-labels -f elf -o log_i586.o log_i586.nasm
 nasm-0.98.39 $CFLAGS -O999999999 -w+orphan-labels -f bin -o log_i586.bin log_i586.nasm
+nasm-0.98.39 $CFLAGS -O0 -w+orphan-labels -f bin -o log_i586.o0.bin log_i586.nasm
 ndisasm -b 32 log_i586.bin | tail  # For the size.
+if ! cmp log_i586.bin log_i586.o0.bin; then
+  ndisasm-0.98.39 -b 32 log_i586.bin >log_i586.ndisasm
+  ndisasm-0.98.39 -b 32 log_i586.o0.bin >log_i586.o0.ndisasm
+  diff -U3 log_i586.ndisasm log_i586.o0.ndisasm
+fi
 qq xstatic gcc -m32 -Os -W -Wall -s -o test_log_i586.prog test_log.c log_i586.o
 ./test_log_i586.prog
 
