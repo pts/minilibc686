272f93245c2b873b3b0f4d95cb82ccf2524a17f9
Peter Szabo <pts@fazekas.hu>
2023-05-17 03:39:34 +0200

    added autodetection of nasm and disasm to test_demo_hello_linux.sh

--- test_demo_hello_linux.sh
+++ test_demo_hello_linux.sh
@@ -1,15 +1,35 @@
 #! /bin/sh --
+# by pts@fazekas.hu at Wed May 17 03:32:07 CEST 2023
+
+if test "$NASM"; then :
+elif nasm-0.98.39 -h 2>/dev/null >&2; then NASM=nasm-0.98.39
+elif nasm -h 2>/dev/null >&2; then NASM=nasm
+else NASM=nasm  # Will fail.
+fi
+
+if test "$NDISASM"; then :
+elif ndisasm-0.98.39 -h 2>/dev/null >&2; then NDISASM=ndisasm-0.98.39
+elif ndisasm -h 2>/dev/null >&2; then NDISASM=ndisasm
+else NDISASM=  # Disabled.
+fi
+
 set -ex
 
 CFLAGS="${*:-}"
-#nasm-0.98.39 $CFLAGS -O999999999 -w+orphan-labels -f elf -o demo_hello_linux.o demo_hello_linux.nasm
-nasm-0.98.39 $CFLAGS -O999999999 -w+orphan-labels -f bin -o demo_hello_linux.elf demo_hello_linux.nasm
-nasm-0.98.39 $CFLAGS -O0 -w+orphan-labels -f bin -o demo_hello_linux.o0.elf demo_hello_linux.nasm
-ndisasm -b 32 demo_hello_linux.elf | tail  # For the size.
+#$NASM $CFLAGS -O999999999 -w+orphan-labels -f elf -o demo_hello_linux.o demo_hello_linux.nasm
+$NASM $CFLAGS -O999999999 -w+orphan-labels -f bin -o demo_hello_linux.elf demo_hello_linux.nasm
+$NASM $CFLAGS -O0 -w+orphan-labels -f bin -o demo_hello_linux.o0.elf demo_hello_linux.nasm
+if test "$NDISASM"; then
+  $NDISASM -b 32 demo_hello_linux.elf | tail  # For the size.
+fi
 if ! cmp demo_hello_linux.elf demo_hello_linux.o0.elf; then
-  ndisasm-0.98.39 -b 32 demo_hello_linux.elf >demo_hello_linux.ndisasm
-  ndisasm-0.98.39 -b 32 demo_hello_linux.o0.elf >demo_hello_linux.o0.ndisasm
-  diff -U3 demo_hello_linux.ndisasm demo_hello_linux.o0.ndisasm
+  if test "$NDISASM"; then
+    $NDISASM -b 32 -e 0x54 demo_hello_linux.elf >demo_hello_linux.ndisasm
+    $NDISASM -b 32 -e 0x54 demo_hello_linux.o0.elf >demo_hello_linux.o0.ndisasm
+    diff -U3 demo_hello_linux.ndisasm demo_hello_linux.o0.ndisasm
+  fi
+  : error: nasm optimization diff found.
+  exit 2
 fi
 chmod +x demo_hello_linux.elf
 ./demo_hello_linux.elf  # Prints: Hello, World!
