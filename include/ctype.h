b1c5acd1cb4a4195100e908e1e93a9b90f0bac30
Peter Szabo <pts@fazekas.hu>
2023-05-21 16:59:19 +0200

    added isalpha_rp1.nasm, without tests

--- include/ctype.h
+++ include/ctype.h
@@ -6,6 +6,8 @@ int isalpha(int c) __asm__("mini_isalpha");
 int isdigit(int c) __asm__("mini_isdigit");
 int isxdigit(int c) __asm__("mini_isxdigit");
 int isspace(int c) __asm__("mini_isspace");
-#endif  /* __UCLIBC__ */
+#else  /* __UCLIBC__ */
+int isalpha(int c) __asm__("mini_isalpha_RP1") __attribute__((__regparm__(1)));
+#endif  /* else __UCLIBC__ */
 
 #endif  /* _CTYPE_H */
