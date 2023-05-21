0a2ec46895369d9abb373fba27ab021b3c06caf6
Peter Szabo <pts@fazekas.hu>
2023-05-21 13:35:00 +0200

    added __attribute__((__noreturn__)) for _exit(3) and exit(3)

--- include/unistd.h
+++ include/unistd.h
@@ -3,8 +3,8 @@
 
 #define NULL ((void*)0)
 
-void _exit(int exit_code) __asm__("mini__exit");  /* Doesn't flush stdio streams first. */
-void exit(int exit_code) __asm__("mini_exit");  /* Flushes stdio streams first. */
+void _exit(int exit_code) __asm__("mini__exit") __attribute__((__noreturn__));  /* Doesn't flush stdio streams first. */
+void exit(int exit_code) __asm__("mini_exit") __attribute__((__noreturn__));  /* Flushes stdio streams first. */
 int close(int fd) __asm__("mini_close");
 ssize_t read(int fd, void *buf, size_t count) __asm__("mini_read");
 ssize_t write(int fd, const void *buf, size_t count) __asm__("mini_write");
