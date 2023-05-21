e5f0ed7a6d347f20f7c1104fc68a645b4d695151
Peter Szabo <pts@fazekas.hu>
2023-05-21 19:26:09 +0200

    added toupper_rp1.nasm, without tests

--- include/ctype.h
+++ include/ctype.h
@@ -7,12 +7,14 @@ int isdigit(int c) __asm__("mini_isdigit");
 int isxdigit(int c) __asm__("mini_isxdigit");
 int isspace(int c) __asm__("mini_isspace");
 int tolower(int c) __asm__("mini_tolower");
+int toupper(int c) __asm__("mini_toupper");
 #else  /* __UCLIBC__ */
 int isalpha(int c) __asm__("mini_isalpha_RP1") __attribute__((__regparm__(1)));
 int isdigit(int c) __asm__("mini_isdigit_RP1") __attribute__((__regparm__(1)));
 int isxdigit(int c) __asm__("mini_isxdigit_RP1") __attribute__((__regparm__(1)));
 int isspace(int c) __asm__("mini_isspace_RP1") __attribute__((__regparm__(1)));
 int tolower(int c) __asm__("mini_tolower_RP1") __attribute__((__regparm__(1)));
+int toupper(int c) __asm__("mini_toupper_RP1") __attribute__((__regparm__(1)));
 #endif  /* else __UCLIBC__ */
 
 #endif  /* _CTYPE_H */
