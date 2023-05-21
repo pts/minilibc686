1dddb4e2cbb89a038e5b906f1626c435882170ba
Peter Szabo <pts@fazekas.hu>
2023-05-21 17:03:46 +0200

    added isxdigit_rp1.nasm, without tests

--- include/ctype.h
+++ include/ctype.h
@@ -9,6 +9,7 @@ int isspace(int c) __asm__("mini_isspace");
 #else  /* __UCLIBC__ */
 int isalpha(int c) __asm__("mini_isalpha_RP1") __attribute__((__regparm__(1)));
 int isdigit(int c) __asm__("mini_isdigit_RP1") __attribute__((__regparm__(1)));
+int isxdigit(int c) __asm__("mini_isxdigit_RP1") __attribute__((__regparm__(1)));
 #endif  /* else __UCLIBC__ */
 
 #endif  /* _CTYPE_H */
