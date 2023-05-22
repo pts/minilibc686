8d67806a0bc38c08e332ef86d568ba4df0b368da
Peter Szabo <pts@fazekas.hu>
2023-05-22 16:48:34 +0200

    made it compile with `gcc -ansi -pedantic' (and added one such invocation), and added __extension__ for 64-bit int typedefs

--- include/stdint.h
+++ include/stdint.h
@@ -5,10 +5,10 @@
 typedef signed char int8_t;
 typedef short int int16_t;
 typedef int int32_t;
-typedef long long int int64_t;
+__extension__ typedef long long int int64_t;  /* __extension__ is to make it work with `gcc -ansi -pedantic'. */
 typedef unsigned char uint8_t;
 typedef unsigned short int uint16_t;
 typedef unsigned int uint32_t;
-typedef unsigned long long int uint64_t;
+__extension__ typedef unsigned long long int uint64_t;  /* __extension__ is to make it work with `gcc -ansi -pedantic'. */
 
 #endif  /* _STDINT_H */
--- include/sys/types.h
+++ include/sys/types.h
@@ -4,7 +4,7 @@
 typedef unsigned size_t;
 typedef int ssize_t;
 typedef long off_t;  /* Still 32 bits only. */
-typedef long long off64_t;
+__extension__ typedef long long off64_t;  /* __extension__ is to make it work with `gcc -ansi -pedantic'. */
 typedef unsigned mode_t;
 
 #endif  /* _SYS_TYPES_H */
