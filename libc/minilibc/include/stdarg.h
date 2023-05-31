3eb4d07ff3a92f9d43f97960851850b3e3598524
Peter Szabo <pts@fazekas.hu>
2023-05-31 15:44:20 +0200

    bugfix: fixed Clang warnings in <stdarg.h>

--- libc/minilibc/include/stdarg.h
+++ libc/minilibc/include/stdarg.h
@@ -8,7 +8,9 @@
  */
 #ifdef __i386__
   typedef char *small_va_list;
-  __extension__ typedef __libc__small_va_list small_va_list;  /* Make sure it's the same as in <stdarg_internal.h> */
+#  ifndef __clang__
+    __extension__ typedef __libc__small_va_list small_va_list;  /* Make sure it's the same as in <stdarg_internal.h> */
+#  endif
 #  define small_va_start(ap, last) ((ap) = (char*)&(last) + ((sizeof(last)+3)&~3), (void)0)  /* i386 only. */
 #  define small_va_arg(ap, type) ((ap) += (sizeof(type)+3)&~3, *(type*)((ap) - ((sizeof(type)+3)&~3)))  /* i386 only. */
 #  define small_va_copy(dest, src) ((dest) = (src), (void)0)  /* i386 only. */
@@ -17,7 +19,9 @@
 
 #ifdef __GNUC__
   typedef __builtin_va_list va_list;
-  __extension__ typedef __libc__va_list va_list;  /* Make sure it's the same as in <stdarg_internal.h> */
+#  ifndef __clang__
+    __extension__ typedef __libc__va_list va_list;  /* Make sure it's the same as in <stdarg_internal.h> */
+#  endif
 #  define va_start(v,l)	__builtin_va_start(v,l)
 #  define va_end(v)	__builtin_va_end(v)
 #  define va_arg(v,l)	__builtin_va_arg(v,l)
@@ -25,7 +29,9 @@
 #else
 #  ifdef __i386__
     typedef small_va_list va_list;
-    __extension__ typedef __libc__small_va_list va_list;  /* Make sure it's the same as in <stdarg_internal.h> */
+#    ifndef __clang__
+      __extension__ typedef __libc__small_va_list va_list;  /* Make sure it's the same as in <stdarg_internal.h> */
+#    endif
 #    define va_start(ap, last) small_va_start(ap, last)
 #    define va_arg(ap, type)   small_va_arg(ap, type)
 #    define va_copy(dest, src) small_va_copy(dest, src)
