b8a6da2d37146a5e62f5939033e87cd77665221f
Peter Szabo <pts@fazekas.hu>
2023-06-06 10:51:01 +0200

    added thorough memcmp checks for mmap(2) tests

--- test/test_mmap_syscalln_linux.c
+++ test/test_mmap_syscalln_linux.c
@@ -4,11 +4,14 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
-const char big[0x4000] = {'B', 'o', 'o', 'm'};  /* A relatively large array. */
+const struct {
+  char padding[0x1000];  /* Make sure that msg is not in the first 0x1000  bytes of the file. */
+  char msg[0x2000];
+} mydata = {{0}, "Catch me if you can!"};  /* Large so that the mapping is long enough. */
 
 int main(int argc, char **argv) {
   int fd;
-  char *p;
+  char *p, *q;
   (void)argc; (void)argv;
   p = (char*)syscall6(SYS_mmap2, 0, 0x3000, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
   if (!p) return 2;
@@ -24,6 +27,11 @@ int main(int argc, char **argv) {
   if (syscall1(SYS_close, fd)) return 9;
   /* Find the string "Boom" in the memory-mapped image. */
   if (memcmp(p, "\x7f""ELF", 4) == 0) return 10;  /* Must not be the beginning of the program file. */
+  /* Find the string msg in the memory-mapped image. */
+  for (q = p + ((unsigned)mydata.msg & 0xfff); q < p + 0x3000; q += 0x1000) {
+    if (memcmp(q, mydata.msg, 64) == 0) break;
+  }
+  if (q >= p + 0x3000) return 11;  /* msg not found. */
   if (syscall2(SYS_munmap, (size_t)p, 0x3000)) return 11;
   return 0;
 }
