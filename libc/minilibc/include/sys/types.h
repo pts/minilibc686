9801bb7d34425464aca34bba9cee9a5e2b376ebb
Peter Szabo <pts@fazekas.hu>
2023-06-10 00:48:12 +0200

    bugfix: fixed type of gid_to to (the correct) unsigned

--- libc/minilibc/include/sys/types.h
+++ libc/minilibc/include/sys/types.h
@@ -10,7 +10,7 @@ __extension__ typedef long long off64_t;  /* __extension__ is to make it work wi
 typedef unsigned mode_t;
 typedef int ptrdiff_t;
 typedef unsigned uid_t;
+typedef unsigned gid_t;
 typedef int pid_t;
-typedef int gid_t;
 
 #endif  /* _SYS_TYPES_H */
