483e28eeabaeec021b7b4b91656f506138be0034
Peter Szabo <pts@fazekas.hu>
2023-06-19 12:40:19 +0200

    added test/demo_stdc.c

---
+++ test/demo_stdc.c
@@ -0,0 +1,17 @@
+/* Compile with `cc -E', to get preprocessor output. */
+#ifdef __STDC__
+STDC: __STDC__
+#else
+#else
+NO STDC
+#endif
+#ifdef __STRICT_ANSI__
+STRICT_ANSI: __STRICT_ANSI__
+#else
+NO STRICT_ANSI
+#endif
+#ifdef __STDC_VERSION__
+STDC_VERSION: __STDC_VERSION__
+#else
+NO STDC_VERSION
+#endif
