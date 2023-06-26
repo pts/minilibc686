987351947acfcfa6c712b83bfe93a173053a91c2
Peter Szabo <pts@fazekas.hu>
2023-06-26 09:48:15 +0200

    added offsetof, without tests

--- libc/minilibc/include/stddef.h
+++ libc/minilibc/include/stddef.h
@@ -6,4 +6,11 @@
 
 #define NULL ((void*)0)  /* Defined in multiple .h files: https://en.cppreference.com/w/c/types/NULL */
 
+#undef offsetof
+#if defined(__GNUC__) && __GNUC__ >= 3  /* __PCC__ has it (and it is used here). __TINYC__ and __WATCOMC__ don't have it. */
+#  define offsetof(type,member) __builtin_offsetof(type,member)
+#else
+#  define offsetof(type,member) ((size_t) &((type*)0)->member)
+#endif
+
 #endif  /* _STDDEF_H */
