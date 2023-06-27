19c2027a4cc0c47caa2de8a35f0af7a4fe8d230f
Peter Szabo <pts@fazekas.hu>
2023-06-27 12:25:02 +0200

    added typedefs for quad_t and u_quad_t

--- libc/minilibc/include/sys/types.h
+++ libc/minilibc/include/sys/types.h
@@ -12,5 +12,7 @@ typedef int ptrdiff_t;
 typedef unsigned uid_t;
 typedef unsigned gid_t;
 typedef int pid_t;
+__extension__ typedef long long quad_t;  /* Always 64-bit. */
+__extension__ typedef unsigned long long u_quad_t;  /* Always 64-bit. */
 
 #endif  /* _SYS_TYPES_H */
