78e79119cccbfe953d302434358a52bf29f5b80d
Peter Szabo <pts@fazekas.hu>
2024-03-13 15:50:22 +0100

    updated fyi/qsort_stable_analyze.py

--- fyi/qsort_stable_analyze.py
+++ fyi/qsort_stable_analyze.py
@@ -55,7 +55,7 @@ import sys
 #
 # If n <= 1: T(n) == 0.
 # If n == 2: T(n) == 1.
-# If n > 2: T(n) <= v(n) + T(x(n)) + T(y(n))
+# If n > 2: T(n) <= v(n) + T(x(n)) + T(n - x(n))
 #
 # CALCT1: [0, 0, 1, 4, 7, 11, 16, 18, 23, 26, 32, 36, 39]
 #
@@ -95,12 +95,11 @@ import sys
 #          z >= 0.9367934091735566
 #          z >= 0.9368  # !! This is too small, it should be about 0.9522.
 #
+# Let K(n) be the largest power of smaller than n. Examples: K(15) == K(16) == 8, K(17) == 16.
 #
-# Mergesort of: S(2**k):
-# 1: 0
-# 2: T(2) <= 1
-# 4: 2*T(2)+T(4) <= 9
-# 8: 4*T(2)+2*T(4)+T(8) <= 41
+# Let S(n) be the maximum number of swaps in an in-place mergesort fo size n:
+#
+# S(n) <= S(K(n)) + S(n - K(n)) + T(n).
 #
 # S(2**k) <= 2*S(2**(k-1)) + T(2**k) if k >= 1.
 #
@@ -109,9 +108,49 @@ import sys
 #         == sum(2**(k-i) * z*(2**i)*log2(2**i), i=0..k)
 #         == 2**k*z * sum(i, i=0..k) == z*(2**k)*k*(k+1)/2
 #         <= z*(2**k)*k*k.
+#
+# !! Prove that S(n) <= z * n * log2(n) * log2(n).
+# It has been proven if n is a power of 2.
+#
+# ---
+#
+# k(n) := ceil(log2(n // 2 + 1)).
+#
+# k(n) is the number of comparisons in the binary search of the in-place merge.
+#
+# Let D(n) be the maximum number of comparisons in an in-place merge of total size n.
+#
+# If n <= 1: D(n) == 0.
+# If n == 2: D(n) == 1.
+# If n >= 3: D(n) <= k(n) + D(x(n)) - D(n - x(n)).
+# !! Prove that splits more even than x(n) are yield smaller recursive D + D.
+#
+# CALCD1: [0, 0, 1, 2, 4, 6, 8, 9, 12, 13, 16, 17, 18, 21, 22, 24, 26, 29, 30, 32, 34, 36, 39, 41, 42, 44, 46, 47, 49, 52, 54, 57, 59]
+# !! Calculate the extra comparison for the shortcut.
+#
+# n   k  x  y  D(n)<=
+# ------------------------------
+#  0  -  -  -   0
+#  1  -  -  -   0
+#  2  -  -  -   1
+#  3  1  1  2   2 == 1+D(1)+D(2)
+#  4  2  1  3   4 == 2+D(1)+D(3)
+#  5  2  1  4   6 == 2+D(1)+D(4)
+#  6  2  1  5   8 == 2+D(1)+D(5)
+#  7  2  2  5   9 == 2+D(2)+D(5)
+#  8  3  2  6  12 == 3+D(2)+D(6)
+#  9  3  2  7  13 == 3+D(2)+D(7)
+# 10  3  2  8  16 == 3+D(2)+D(8)
+# 11  3  3  8  17 == 3+D(3)+D(8)
+# 12  3  3  9  18 == 3+D(3)+D(9)
+#
+# Let C(n) be the maximum number of comparisons in an in-place mergesort fo size n:
+#
+# C(n) <= C(K(n)) + C(n - K(n)) + D(n).
+#
 
 def calct1(n, _cache=[0, 0, 1]):
-  """Returns upper bound on maximum number of swaps in an in-place merge of size n."""
+  """Returns upper bound on maximum number of swaps in an in-place merge of total size n."""
   if n < 0:
     raise ValueError
   while len(_cache) <= n:
@@ -130,7 +169,7 @@ def calct2(n, _cache=[0, 0, 1]):
 
 
 def calcs1(n, _cache=[0, 0]):
-  """Returns upper bound on maximum number of swaps in a mergesort of size n."""
+  """Returns upper bound on maximum number of swaps in an in-place mergesort of size n."""
   if n < 0:
     raise ValueError
   while len(_cache) <= n:
@@ -139,6 +178,33 @@ def calcs1(n, _cache=[0, 0]):
     while kk << 1 < nn:
       kk <<= 1
     _cache.append(_cache[kk] + _cache[nn - kk] + calct1(nn))
+  return _cache[n]
+
+
+def calcd1(n, _cache=[0, 0, 1]):
+  """Returns upper bound on maximum number of comparisons in an in-place merge of total size n."""
+  if n < 0:
+    raise ValueError
+  while len(_cache) <= n:
+    nn = len(_cache)
+    nnn = (n >> 1) + 1
+    k = 0
+    while (1 << k) < nnn:
+      k += 1
+    _cache.append(k + _cache[(nn + 1) >> 2] + _cache[nn - ((nn + 1) >> 2)])
+  return _cache[n]
+
+
+def calcc1(n, _cache=[0, 0]):
+  """Returns upper bound on maximum number of comparisons in an in-place mergesort of size n."""
+  if n < 0:
+    raise ValueError
+  while len(_cache) <= n:
+    nn = len(_cache)
+    kk = 1
+    while kk << 1 < nn:
+      kk <<= 1
+    _cache.append(_cache[kk] + _cache[nn - kk] + calcd1(nn))
   return _cache[n]
 
 
@@ -192,6 +258,56 @@ def main(argv):
         maxr1 = r1
         print (n, s1, u1, r1)  # r1 seems so be limised.
     # It seems that S(n) < 0.75 * n * log2(n) * log2(n) for any n >= 2. And S(n) == 0 for n < 2.
+  print 'CALCD1:', [calcd1(n) for n in xrange(33)]
+  assert [calcd1(n) for n in xrange(33)] == [0, 0, 1, 2, 4, 6, 8, 9, 12, 13, 16, 17, 18, 21, 22, 24, 26, 29, 30, 32, 34, 36, 39, 41, 42, 44, 46, 47, 49, 52, 54, 57, 59]
+
+  print 'CALCC1:', [calcc1(n) for n in xrange(33)]
+  assert [calcc1(n) for n in xrange(33)] == [0, 0, 1, 3, 6, 12, 15, 18, 24, 37, 41, 44, 48, 57, 61, 66, 74, 103, 105, 109, 114, 122, 128, 133, 140, 155, 161, 165, 171, 183, 189, 197, 207]
+  if 1:
+    for n in xrange(2, 33):
+      c1 = calcc1(n)
+      u1 = n * ((math.log(n) / math.log(2)))
+      print (n, c1, u1, c1 / u1)
+    print '---'
+    maxr1 = 0
+    for n in xrange(2, 33):
+      c1 = calcc1(n)
+      u1 = n * ((math.log(n) / math.log(2)))
+      r1 = c1 / u1
+      if r1 > maxr1:
+        maxr1 = r1
+        print (n, c1, u1, r1)  # r1 seems co be limiced.
+    print '---'
+    maxr1 = 0
+    for n in xrange(33, 10000):
+      c1 = calcc1(n)
+      u1 = n * ((math.log(n) / math.log(2)))
+      r1 = c1 / u1
+      if r1 > maxr1:
+        maxr1 = r1
+        print (n, c1, u1, r1)  # r1 seems co be limiced.
+    print '---'
+    maxr1 = 0
+    for k in xrange(1, 70):
+      n = (1 << k) + 1
+      c1 = calcc1(n)
+      u1 = n * ((math.log(n) / math.log(2)))
+      r1 = c1 / u1
+      if r1 > maxr1:
+        maxr1 = r1
+        # (8193, 200461, 106510.44278309244, 1.8820783649189876)
+        # (16385, 433962, 229391.44273906754, 1.8917968116780677)
+        # (32769, 934593, 491536.4427170545, 1.901370720009837)
+        # (65537, 2002784, 1048593.4427060478, 1.9099718903749052)
+        # (131073, 4269122, 2228242.4427005444, 1.9159144975382427)
+        # (262145, 9077484, 4718611.442697792, 1.9237617062213732)
+        # (524289, 19200302, 9961492.442696417, 1.9274523481747263)
+        # (1048577, 40571133, 20971541.44269573, 1.9345803984346939)
+        # (2097153, 85306297, 44040214.44269539, 1.9370091194946282)
+        # (4194305, 179317875, 92274711.44269522, 1.9433046410701664)
+        # (8388609, 375295641, 192938008.44269514, 1.9451617855352084)
+        # (16777217, 785371931, 402653209.4426951, 1.950492166912115)
+        print (n, c1, u1, r1)  # r1 seems co be limiced.
 
 
 if __name__ == '__main__':
