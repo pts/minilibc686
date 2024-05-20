a5bb2f9d73bad59224509132cf9e840c8dbd458c
Peter Szabo <pts@fazekas.hu>
2024-05-20 12:19:20 +0200

    made test/test_strtold.c pass with GCC, PCC and TCC

--- test/test_strtod.c
+++ test/test_strtod.c
@@ -2,13 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#if defined(__TINYC__) && defined(__UCLIBC__)
-#  define __builtin_isnan(x) isnan(x)
+#ifdef __UCLIBC__
+#  define mini_strtod strtod
+#else
+  extern double mini_strtod(const char *nptr, char **endptr);  /* Function under test. */
 #endif
 
-extern double mini_strtod(const char *nptr, char **endptr);  /* Function under test. */
-
-#define FP_EQ(a, b) ((a) == (b) || (__builtin_isnan(a) && __builtin_isnan(b)))
+static char FP_EQ(double a, double b) {
+  union { unsigned u[2]; double d; } xa, xb;
+  xa.d = a;
+  xb.d = b;
+  return xa.u[0] == xb.u[0] && xa.u[1] == xb.u[1];
+}
 
 static char expect(const char *nptr) {
   char *expected_endptr;
@@ -20,6 +25,15 @@ static char expect(const char *nptr) {
   const double value2 = mini_strtod(nptr, NULL);
   char is_ok = (FP_EQ(value, expected_value) && size == expected_size && FP_EQ(value, expected_value));
   printf("is_ok=%d str=(%s) expected_value=%.17g expected_size=%d value=%.17g size=%d value2=%.17g\n", is_ok, nptr, expected_value, expected_size, value, size, value2);
+  return is_ok;
+}
+
+static char expect2(const char *nptr, unsigned u0, unsigned u1) {
+  union { unsigned u[2]; double d; } x;
+  char is_ok;
+  x.d = mini_strtod(nptr, NULL);
+  is_ok = x.u[0] == u0 && x.u[1] == u1;
+  printf("is_ok=%d value=%.17g expected_u={0x%x,0x%x} u={0x%x,0x%x}\n", is_ok, x.d, u0, u1, x.u[0], x.u[1]);
   return is_ok;
 }
 
@@ -38,5 +52,6 @@ int main(int argc, char **argv) {
   if (!expect("+infhello")) exit_code |= 1;
   if (!expect("+inhello")) exit_code |= 1;
   if (!expect("+infiniTyLong")) exit_code |= 1;
+  if (!expect2("2.2250738585072014e-308", 0, 0x100000)) exit_code |= 1;
   return exit_code;
 }
--- test/test_strtold.c
+++ test/test_strtold.c
@@ -15,12 +15,46 @@ static float fadd2(long double a, long double b) {
 }
 
 int main(int argc, char **argv) {
-  union { unsigned u[3]; long double ld; } x;
+  union { unsigned u[3]; long double ld; double d; } x, ldbl_max, ldbl_min, ldbl_epsilon, dbl_min, dbl_mind;
   char is_ok, is_all_ok = 1;
   (void)argc; (void)argv;
   x.u[2] = 0;
 
-  x.ld = (double)LDBL_MIN;
+  ldbl_max.u[0] = ldbl_max.u[1] = -1; ldbl_max.u[2] = 0x7ffe;
+  ldbl_min.u[0] = 0; ldbl_min.u[1] = 0x80000000U; ldbl_min.u[2] = 1;
+  ldbl_epsilon.u[0] = 0; ldbl_epsilon.u[1] = 0x80000000U; ldbl_epsilon.u[2] = 0x3fc0;
+  dbl_min.u[0] = 0; dbl_min.u[1] = 0x80000000U; dbl_min.u[2] = 0x3c01;
+  dbl_mind.u[0] = 0; dbl_mind.u[1] = 0x100000;
+
+#ifndef __PCC__  /* !! Is it PCC uClibc 0.9.30.1 strtod(...) and strtold(...) bug. */
+  x.ld = 1.0842021724855044340e-19L;  /* LDBL_EPSILON. */
+  is_all_ok &= (is_ok = x.u[0] == 0 && x.u[1] == 0x80000000U && (x.u[2] & 0xffff) == 0x3fc0);
+  printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
+
+  x.ld = 2.2250738585072014e-308;  /* DBL_MIN. */
+  is_all_ok &= (is_ok = x.u[0] == 0 && x.u[1] == 0x80000000U && (x.u[2] & 0xffff) == 0x3c01);
+  printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
+
+  x.d = 2.2250738585072014e-308;  /* DBL_MIN. */
+  is_all_ok &= (is_ok = x.u[0] == 0 && x.u[1] == 0x100000);
+  printf("is_ok=%d u0=0x%x u1=0x%x\n", is_ok, x.u[0], x.u[1]);
+#endif
+
+#ifndef __TINYC__  /* !! TCC (long double) --> (double) conversion bug. GCC and PCC work fine. */
+  x.ld = (double)ldbl_min.ld;
+  is_all_ok &= (is_ok = x.ld == 0.0L);
+  printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
+
+  x.ld = fadd2(1, 1.000000000000001L);
+  is_all_ok &= (is_ok = x.ld == 2.0L);
+  printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
+
+  x.ld = mini_strtold("2.2250738585072014e-308", 0);
+  is_all_ok &= (is_ok = (double)x.ld == dbl_mind.d);
+  printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
+#endif
+
+  x.ld = (double)3.3621031431120935062e-4932L;
   is_all_ok &= (is_ok = x.ld == 0.0L);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
@@ -28,100 +62,84 @@ int main(int argc, char **argv) {
   is_all_ok &= (is_ok = x.ld == 1.0L);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
-#ifndef __TINYC__
-  x.ld = fadd2(1, 1.000000000000001L);
-  is_all_ok &= (is_ok = x.ld == 2.0L);  /* !! TCC bug. GCC and PCC work file. */
-  printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
-#endif
-
   x.ld = mini_strtold("2.22507385850720138309e-308", 0);
-  is_all_ok &= (is_ok = x.ld == (long double)DBL_MIN && x.ld == DBL_MIN && (double)x.ld == DBL_MIN);
+  is_all_ok &= (is_ok = x.ld == dbl_min.ld && (double)x.ld == dbl_mind.d);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
   x.ld = mini_strtold("2.2250738585072014e-308", 0);
-  is_all_ok &= (is_ok = (double)x.ld == DBL_MIN && x.ld > (long double)DBL_MIN && x.u[0] == 0x46 && x.u[1] == 0x80000000U && (x.u[2] & 0xffff) == 0x3c01);
+  is_all_ok &= (is_ok = x.ld > dbl_min.ld && x.u[0] == 0x46 && x.u[1] == 0x80000000U && (x.u[2] & 0xffff) == 0x3c01);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
   x.ld = mini_strtold("inf", 0);
-  is_all_ok &= (is_ok = x.ld > 0.0L && x.ld > LDBL_MAX && x.ld == x.ld * .5);
+  is_all_ok &= (is_ok = x.ld > 0.0L && x.ld > ldbl_max.ld && x.ld == x.ld * .5);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
   x.ld = mini_strtold("infinity", 0);
-  is_all_ok &= (is_ok = x.ld > 0.0L && x.ld > LDBL_MAX && x.ld == x.ld * .5);
+  is_all_ok &= (is_ok = x.ld > 0.0L && x.ld > ldbl_max.ld && x.ld == x.ld * .5);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
   x.ld = mini_strtold("-infinity", 0);
-  is_all_ok &= (is_ok = x.ld < 0.0L && x.ld < -LDBL_MAX && x.ld == x.ld * .5);
+  is_all_ok &= (is_ok = x.ld < 0.0L && x.ld < -ldbl_max.ld && x.ld == x.ld * .5);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
-#ifndef __TINYC__  /* !! */
   x.ld = mini_strtold("1.18973149535723176502126385303097021e+4932", 0);
-  is_all_ok &= (is_ok = x.ld == LDBL_MAX);
+  is_all_ok &= (is_ok = x.ld == ldbl_max.ld);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
   x.ld = mini_strtold("1.189731495357231765e+4932", 0);
-  is_all_ok &= (is_ok = x.ld == LDBL_MAX);
+  is_all_ok &= (is_ok = x.ld == ldbl_max.ld);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
   x.ld = mini_strtold("1.1897314953572317650E4932", 0);
-  is_all_ok &= (is_ok = x.ld == LDBL_MAX);
+  is_all_ok &= (is_ok = x.ld == ldbl_max.ld);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
   x.ld = mini_strtold("1.18973149535723176498902e+4932", 0);
-  is_all_ok &= (is_ok = x.ld == LDBL_MAX);
+  is_all_ok &= (is_ok = x.ld == ldbl_max.ld);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
   x.ld = mini_strtold("1.18973149535723176498901e+4932", 0);
-  is_all_ok &= (is_ok = x.ld > 0.0L && x.ld < LDBL_MAX);
+  is_all_ok &= (is_ok = x.ld > 0.0L && x.ld < ldbl_max.ld);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
-#endif
   x.ld = mini_strtold("1.189731495357231766e+4932", 0);
-  is_all_ok &= (is_ok = x.ld > LDBL_MAX && x.ld == x.ld * .5);  /* Infinity. */
+  is_all_ok &= (is_ok = x.ld > ldbl_max.ld && x.ld == x.ld * .5);  /* Infinity. */
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
-#ifndef __TINYC__  /* !! */
   x.ld = mini_strtold(" \t3.36210314311209350626267781732175260e-4932", 0);
-  is_all_ok &= (is_ok = x.ld > 0.0L && x.ld == LDBL_MIN && x.ld < DBL_MIN);
+  is_all_ok &= (is_ok = x.ld > 0.0L && x.ld == ldbl_min.ld && x.ld < dbl_min.ld);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
   x.ld = mini_strtold("3.36210314311209350626267781732175260e-4932", 0);  /* "%.36Lg". */
-  is_all_ok &= (is_ok = x.ld == LDBL_MIN);
+  is_all_ok &= (is_ok = x.ld == ldbl_min.ld);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
-#endif
 
   x.ld = mini_strtold("3.3621031431120935060e-4932", 0);
-  is_all_ok &= (is_ok = x.ld < LDBL_MIN);
+  is_all_ok &= (is_ok = x.ld < ldbl_min.ld);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
-#ifndef __TINYC__  /* !! */
   x.ld = mini_strtold("3.3621031431120935061e-4932", 0);  /* "%.20Lg". */
-  is_all_ok &= (is_ok = x.ld == LDBL_MIN);
+  is_all_ok &= (is_ok = x.ld == ldbl_min.ld);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
   x.ld = mini_strtold("3.3621031431120935062e-4932", 0);
-  is_all_ok &= (is_ok = x.ld == LDBL_MIN);
+  is_all_ok &= (is_ok = x.ld == ldbl_min.ld);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
   x.ld = mini_strtold("3.3621031431120935064e-4932", 0);
-  is_all_ok &= (is_ok = x.ld == LDBL_MIN);
+  is_all_ok &= (is_ok = x.ld == ldbl_min.ld);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
   x.ld = mini_strtold("3.3621031431120935065e-4932", 0);
-  is_all_ok &= (is_ok = x.ld > LDBL_MIN);
-  printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
-#endif
-
-  x.ld = LDBL_EPSILON;
-  is_all_ok &= (is_ok = x.ld == LDBL_EPSILON);
+  is_all_ok &= (is_ok = x.ld > ldbl_min.ld);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
   x.ld = mini_strtold("1.08420217248550443400745280086994171e-19", 0);
-  is_all_ok &= (is_ok = x.ld == LDBL_EPSILON);
+  is_all_ok &= (is_ok = x.ld == ldbl_epsilon.ld);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
   x.ld = mini_strtold("1.0842021724855044340E-19", 0);
-  is_all_ok &= (is_ok = x.ld == LDBL_EPSILON);
+  is_all_ok &= (is_ok = x.ld == ldbl_epsilon.ld);
   printf("is_ok=%d u0=0x%x u1=0x%x u2=0x%x\n", is_ok, x.u[0], x.u[1], x.u[2] & 0xffff);
 
   x.ld = mini_strtold("3.64519953188247460252840593361941982e-4951", 0);
