f6409d1100ae2890ee06e90f6c09d1666a367785
Peter Szabo <pts@fazekas.hu>
2024-06-06 04:59:20 +0200

    size optimization: inlined alignment of alloca(...) to assembly for __WATCOMC__

--- libc/minilibc/include/alloca.h
+++ libc/minilibc/include/alloca.h
@@ -6,9 +6,9 @@
     /* Based on OpenWatcom lh/alloca.h. It also calls stackavail(...) in the
      * OpenWatcom libc, but this libc doesn't have that.
      */
-    extern void *__doalloca(unsigned long /*size_t*/ __size);
-#    pragma aux __doalloca = "sub esp, eax"  __parm __nomemory [__eax]  __value [__esp]  __modify __exact __nomemory [__esp]
-#    define alloca(x) __doalloca((((x)+(sizeof(int)-1))&~(sizeof(int)-1)))
+    extern void *alloca(unsigned long /*size_t*/ __size);
+#    pragma aux alloca = "sub esp, eax" "and esp, -4" __parm __nomemory [__eax] __value [__esp] __modify __exact __nomemory [__esp]
+#    define alloca(x) alloca(x)
 #    define __builtin_alloca(x) alloca(x)  /* For compatibility with GCC. */
 #  elif defined(__builtin_alloca)
 #    ifndef alloca
