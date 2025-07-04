2891cb03d42902f1163d08b4bbe72f7cbccedc61
Peter Szabo <pts@fazekas.hu>
2025-07-05 00:59:57 +0200

    added fyi/try_fflush.c

---
+++ fyi/try_fflush.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  (void)argc; (void)argv;
+  fputc('X', fopen("/dev/null", "w"));
+  fputc('X', fopen("/dev/null", "w"));
+  fputc('X', fopen("/dev/null", "w"));
+  fputc('X', fopen("/dev/null", "w"));
+  putchar('\n');
+  return 0;
+}
